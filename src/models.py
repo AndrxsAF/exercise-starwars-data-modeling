693cebf06705d78d5f6c8a72e98ef97d403c8f9a
AndrxsAF <andrxsaf@gmail.com>
2022-02-05 11:35:10 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Blog_User(Base):
+    __tablename__ = "blog_User"
     id = Column(Integer, primary_key=True)
+    user = Column(String(20), nullable=False, unique=True)
+    first_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+    id = Column(Integer, primary_key=True)
+    id_post = Column(Integer)
+    id_user = Column(Integer, ForeignKey('blog_User.id'))
+    blog_user = relationship(Blog_User)
+
+class Character(Base):
+    __tablename__ = "character"
+    id = Column(Integer, ForeignKey('favorite.id_post'), primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    home_world = Column(String(250), nullable=False)
+    home_world_id = Column(Integer, ForeignKey('planet.id'))
+    favorite = relationship(Favorite)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer, ForeignKey('favorite.id_post'), primary_key=True)
+    name = Column(String(250), nullable=False)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    climate = Column(String(250))
+    gravity = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+    population = Column(Integer)
+    favorite = relationship(Favorite)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
